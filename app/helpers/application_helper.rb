100cfb34bfd57639c25789c69ddedb0160cb6aa1
Xavier Noria <fxn@hashref.com>
2009-03-24 14:19:47 +0100

    added some style

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,7 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
   def link_to_commit(commit)
-    link_to commit.short_hash, commit.github_url
+    link_to commit.short_hash, commit.github_url, :class => 'commit'
   end
 
   def genitiveize(name)
