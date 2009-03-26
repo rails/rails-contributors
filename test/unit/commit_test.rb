7414f46145e1487e7391ae904fd2f7243d22bd9c
Xavier Noria <fxn@hashref.com>
2009-03-27 00:28:07 +0100

    update commit tests after moving special casing elsewhere

--- test/unit/commit_test.rb
+++ test/unit/commit_test.rb
@@ -8,8 +8,8 @@ class CommitTest < ActiveSupport::TestCase
     assert_equal [], commit.extract_svn_contributor_names_from_text("")
     assert_equal [], commit.extract_svn_contributor_names_from_text("nothing here to extract")
     assert_equal ['miloops'], commit.extract_svn_contributor_names_from_text("Fix case-sensitive validates_uniqueness_of. Closes #11366 [miloops]")
-    assert_equal ['Adam Milligan', 'Pratik'], commit.extract_svn_contributor_names_from_text('Ensure methods called on association proxies respect access control. [#1083 state:resolved] [Adam Milligan, Pratik]')
-    assert_equal ['Kevin Clark', 'Jeremy Hopple'], commit.extract_svn_contributor_names_from_text(<<MESSAGE)
+    assert_equal ['Adam Milligan, Pratik'], commit.extract_svn_contributor_names_from_text('Ensure methods called on association proxies respect access control. [#1083 state:resolved] [Adam Milligan, Pratik]')
+    assert_equal ['Kevin Clark & Jeremy Hopple'], commit.extract_svn_contributor_names_from_text(<<MESSAGE)
     * Fix pagination problems when using include
     * Introduce Unit Tests for pagination
     * Allow count to work with :include by using count distinct.
