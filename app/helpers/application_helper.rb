24bf8da5dae3b33e1ef128ca473e9de51918a172
Xavier Noria <fxn@hashref.com>
2009-07-08 00:28:20 +0200

    marks name as RDFa metadata in the page of a contributor

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -29,6 +29,7 @@ module ApplicationHelper
   end
 
   def normalize_title(title)
-    title.starts_with?("Rails Contributors") ? title : "Rails Contributors - #{title}"
+    title = title.starts_with?("Rails Contributors") ? title : "Rails Contributors - #{title}"
+    strip_tags(title)
   end
 end
