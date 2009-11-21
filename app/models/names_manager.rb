0c320b6965708914276f4adb837df4cb649e319c
Xavier Noria <fxn@hashref.com>
2009-11-21 23:25:00 +0100

    maps email address of David Felstead

--- app/models/names_manager.rb
+++ app/models/names_manager.rb
@@ -141,6 +141,7 @@ module NamesManager
   map 'David A. Black',             'dblack'
   map 'David Calavera',             'calavera', 'david.calavera'
   map 'David Dollar',               'ddollar'
+  map 'David Felstead',             "david.felstead\100gmail.com"
   map 'Dave Dribin',                "dave-ml\100dribin.org"
   map 'David Heinemeier Hansson',   'DHH'
   map 'David Morton',               "mortonda\100dgrmm.net"
