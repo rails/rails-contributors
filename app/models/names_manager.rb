463ec889866902e2d891a5816d5f51579073a8a0
Xavier Noria <fxn@hashref.com>
2009-11-21 22:08:04 +0100

    maps email address of Chris Brinker

--- app/models/names_manager.rb
+++ app/models/names_manager.rb
@@ -108,6 +108,7 @@ module NamesManager
   map 'Charles M. Gerungan',        "charles.gerungan\100gmail.com"
   map 'Cheah Chu Yeow',             'Chu Yeow', 'chuyeow'
   map 'Choon Keat',                 "choonkeat\100gmail.com", 'choonkeat'
+  map 'Chris Brinker',              "chris\100chrisbrinker.com"
   map 'Chris Carter',               "cdcarter\100gmail.com"
   map 'Chris Kampmeier',            'kampers'
   map 'Chris McGrath',              "c.r.mcgrath\100gmail.com", 'c.r.mcgrath', "chris\100octopod.info", 'octopod'
