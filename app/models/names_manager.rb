f38b9ebb17b22827c3f9031069530b8a0a7b0f40
Xavier Noria <fxn@hashref.com>
2009-11-21 23:39:58 +0100

    maps email address of Dominic Sisneros

--- app/models/names_manager.rb
+++ app/models/names_manager.rb
@@ -152,6 +152,7 @@ module NamesManager
   map 'Deirdre Saoirse',            "deirdre\100deirdre.net"
   map 'DeLynn Berry',               'Delynn', 'delynnb', 'DeLynn Barry', 'DeLynnB', 'DelynnB', 'DeLynn B', "delynn\100gmail.com"
   map 'Derrick Spell',              "derrickspell\100cdmplus.com"
+  map 'Dominic Sisneros',           "dom\100sisna.com"
   map 'Don Park',                   "don.park\100gmail.com"
   map 'Dr Nic',                     'drnic'
   map 'Duane Johnson',              "duane.johnson\100gmail.com"
