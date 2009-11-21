80209fe26d5cc4f7d4547dabeb5ed883d910799f
Xavier Noria <fxn@hashref.com>
2009-11-21 21:05:21 +0100

    maps email address of Alexander Borovsky

--- app/models/names_manager.rb
+++ app/models/names_manager.rb
@@ -62,6 +62,7 @@ module NamesManager
   map 'Akira Matsuda',              '松田 明'
   map 'alancfrancis',               "alancfrancis\100gmail.com"
   map 'Alex Wayne',                 "rubyonrails\100beautifulpixel.com"
+  map 'Alexander Borovsky',         "alex.borovsky\100gmail.com"
   map 'Alexander Dymo',             'adymo'
   map 'Alexander Staubo',           "alex\100purefiction.net"
   map 'Alexey Zatsepin',            'alexey'
