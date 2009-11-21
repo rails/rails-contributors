b7ecaad5ef34d54c56d070a595c396abd6124578
Xavier Noria <fxn@hashref.com>
2009-11-22 00:14:42 +0100

    adds edward.frederick@... to WAITING_FOR

--- app/models/names_manager.rb
+++ app/models/names_manager.rb
@@ -13,6 +13,7 @@ module NamesManager
     david.a.williams\100gmail.com
     dweitzman\100gmail.com
     dwlt\100dwlt.net
+    edward.frederick\100revolution.com
   )
 
   # I've sent an email to these addresses, and got some sort of error back.
