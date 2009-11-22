f1d11c5d405f9b0a12fb784068a09e1823b13329
Xavier Noria <fxn@hashref.com>
2009-11-22 23:03:33 +0100

    maps handler of Laurel Fan

--- app/models/names_manager.rb
+++ app/models/names_manager.rb
@@ -311,7 +311,7 @@ module NamesManager
   map 'Lars Francke',               "l.francke\100gmx.de"
   map 'Lars Kanis',                 "kanis\100comcard.de"
   map 'Lars Pind',                  "lars\100pinds.com", "lars\100pind.com", 'Lars pind', 'lars pind'
-  map 'Laurel Fan',                 "laurel\100gorgorg.org"
+  map 'Laurel Fan',                 "laurel\100gorgorg.org", 'laurelfan'
   map 'Lawrence Pit',               'lawrence'
   map 'Lee Marlow',                 'lmarlow', "lmarlow\100yahoo.com"
   map "Lee O'Mara",                 "lee\100omara.ca"
