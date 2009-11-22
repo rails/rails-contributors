87d926acda0013e263b79ebb1d1ae9ba6d1aa1d0
Xavier Noria <fxn@hashref.com>
2009-11-22 23:08:07 +0100

    maps handler of Jonathan Garvin

--- app/models/names_manager.rb
+++ app/models/names_manager.rb
@@ -267,6 +267,7 @@ module NamesManager
   map 'Jon Moses',                  "jon\100burningbush.us"
   map 'Jon Olson',                  "jsolson\100damogran.org"
   map 'Jon Wood',                   "jon\100instance-design.co.uk", "jon\100blankpad.net"
+  map 'Jonathan Garvin',            "jsgarvin"
   map 'Jonathan Leighton',          "turnip\100turnipspatch.com"
   map 'Jonathan del Strother',      'Catfish', 'catfish'
   map 'Jonathan Viney',             "jonathan\100bluewire.net.nz"
