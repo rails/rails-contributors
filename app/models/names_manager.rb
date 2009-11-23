225043e1a0d941b8d647b431dd134d7019ad830a
Xavier Noria <fxn@hashref.com>
2009-11-23 22:49:07 +0100

    maps email address of Michael Daines

--- app/models/names_manager.rb
+++ app/models/names_manager.rb
@@ -372,6 +372,7 @@ module NamesManager
   map 'Matthew Rudy Jacobs',        'MatthewRudy'
   map 'Matthew Walker',             "matthew\100walker.wattle.id.au"
   map 'Micah Alles',                "alles\100atomicobject.com"
+  map 'Michael Daines',             "me\100mdaines.com"
   map 'Michael Galero',             'mikong'
   map 'Michael Klishin',            'antares', 'Michael S. Klishin'
   map 'Michael Koziarski',          'Koz', 'nzkoz'
