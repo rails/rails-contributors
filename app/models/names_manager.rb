0fe1a979d4f2ec84fa80f2bf3c9ed530e17410fe
Xavier Noria <fxn@hashref.com>
2009-11-23 13:49:23 +0100

    maps handler of Andrew Chase

--- app/models/names_manager.rb
+++ app/models/names_manager.rb
@@ -99,6 +99,7 @@ module NamesManager
   map 'Alexey Zatsepin',            'alexey'
   map 'Aliaksey Kandratsenka',      'Aleksey Kondratenko'
   map 'Andrew A. Smith',            "andy\100tinnedfruit.org"
+  map 'Andrew Chase',               'acechase'  
   map 'Andrew Kaspick',             "akaspick\100gmail.com", 'akaspick', "andrew\100redlinesoftware.com"
   map 'Andrew Peters',              "andrew.john.peters\100gmail.com"
   map 'Andrew White',               'pixeltrix'
