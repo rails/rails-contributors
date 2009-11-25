9bd6a6789852b167df988f925f61163618855d43
Xavier Noria <fxn@hashref.com>
2009-11-25 21:20:51 +0100

    maps email address of okkez

--- app/models/names_manager.rb
+++ app/models/names_manager.rb
@@ -415,6 +415,7 @@ module NamesManager
   map 'Norbert Crombach',           'norbert'
   map 'Norman Elton',               "normelton\100gmail.com"
   map 'Obie Fernandez',             'ObieFernandez'
+  map 'okkez',                      "okkez000\100gmail.com"
   map 'Pat Maddox',                 "pergesu\100gmail.com"
   map 'Patrick Lenz',               "patrick\100lenz.sh"
   map 'Paul Butcher',               "paul\100paulbutcher.com"
