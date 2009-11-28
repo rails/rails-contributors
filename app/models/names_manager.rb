7cbfcc73e05c9cfdf22f94e0a1b8fae5a12037bb
Xavier Noria <fxn@hashref.com>
2009-11-29 00:53:11 +0100

    maps handler of James Cox

--- app/models/names_manager.rb
+++ app/models/names_manager.rb
@@ -295,6 +295,7 @@ module NamesManager
   map 'Jakob Skjerning',            'Jakob S', "jakob\100mentalized.net"
   map 'Jarkko Laine',               "jarkko\100jlaine.net", 'Jarkko', 'jarkko'
   map 'James Adam',                 'lazyatom', "james.adam\100gmail.com"
+  map 'James Cox',                  'imajes'
   map 'James Edward Gray II',       "james\100grayproductions.net"
   map 'James Mead',                 'floehopper'
   map 'James Megquier',             "james\100slashetc.com"
