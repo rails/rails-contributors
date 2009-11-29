413af9030c1ad1e711a88762f23f314403d1e230
Xavier Noria <fxn@hashref.com>
2009-11-29 20:27:32 +0100

    adds several handlers, and structures work in progress regarding emails and github usernames in modules

--- app/models/names_manager.rb
+++ app/models/names_manager.rb
@@ -1,64 +1,93 @@
 require 'set'
 
 module NamesManager
-  # I've sent an email to these email addresses, and there's no response
-  # so far.
-  WAITING_FOR = %W(
-    agkr\100pobox.com
-    imbcmdth\100hotmail.com
-    jhahn\100niveon.com
-    nick+rails\100ag.arizona.edu
-    alec+rails\100veryclever.net
-    alex.r.moon\100gmail.com
-    david.a.williams\100gmail.com
-    dwlt\100dwlt.net
-    edward.frederick\100revolution.com
-    eli.gordon\100gmail.com
-    eugenol\100gmail.com
-    fhanshaw\100vesaria.com
-    gaetanot\100comcast.net
-    gnuman1\100gmail.com
-    info\100loobmedia.com
-    jan\100ulbrich-boerwang.de
-    jonrailsdev\100shumi.org
-    junk\100miriamtech.com
-    justin\100textdrive.com
-    machomagna\100gmail.com
-    me\100jonnii.com
-    minimudboy\100gmail.com
-    rails.20.clarry\100spamgourmet.com
-    rails-bug\100owl.me.uk
-    s.brink\100web.de
-    schultzr\100gmail.com
-    seattle\100rootimage.msu.edu
-    yanowitz-rubyonrails\100quantumfoam.org
-    zaitzow\100gmail.com
-  )
+  module EmailAddresses
+    # I've sent an email to these email addresses, and there's no response
+    # so far.
+    WAITING_FOR = %W(
+      agkr\100pobox.com
+      imbcmdth\100hotmail.com
+      jhahn\100niveon.com
+      nick+rails\100ag.arizona.edu
+      alec+rails\100veryclever.net
+      alex.r.moon\100gmail.com
+      david.a.williams\100gmail.com
+      dwlt\100dwlt.net
+      edward.frederick\100revolution.com
+      eli.gordon\100gmail.com
+      eugenol\100gmail.com
+      fhanshaw\100vesaria.com
+      gaetanot\100comcast.net
+      gnuman1\100gmail.com
+      info\100loobmedia.com
+      jan\100ulbrich-boerwang.de
+      jonrailsdev\100shumi.org
+      junk\100miriamtech.com
+      justin\100textdrive.com
+      machomagna\100gmail.com
+      me\100jonnii.com
+      minimudboy\100gmail.com
+      rails.20.clarry\100spamgourmet.com
+      rails-bug\100owl.me.uk
+      s.brink\100web.de
+      schultzr\100gmail.com
+      seattle\100rootimage.msu.edu
+      yanowitz-rubyonrails\100quantumfoam.org
+      zaitzow\100gmail.com
+    )
 
-  # I've sent an email to these addresses, and got some sort of error back.
-  UNREACHABLE_ADDRESSES = %W(
-    rails\100cogentdude.com
-    rubyonrails\100atyp.de
-    takiuchi\100drecom.co.jp
-    altano\100bigfoot.com
-    asnem\100student.ethz.ch
-    damn_pepe\100gmail.com
-    dev.rubyonrails\100maxdunn.com
-    kdole\100tamu.edu
-    mklame\100atxeu.com
-    nbpwie102\100sneakemail.com
-    nwoods\100mail.com
-    nkriege\100hotmail.com
-    pfc.pille\100gmx.net
-    rcolli2\100tampabay.rr.com
-    solo\100gatelys.com
-    starr\100starrnhorne.com
-    zachary\100panandscan.com
-  )
+    # I've sent an email to these addresses, and got some sort of error back.
+    UNREACHABLE = %W(
+      rails\100cogentdude.com
+      rubyonrails\100atyp.de
+      takiuchi\100drecom.co.jp
+      altano\100bigfoot.com
+      asnem\100student.ethz.ch
+      damn_pepe\100gmail.com
+      dev.rubyonrails\100maxdunn.com
+      kdole\100tamu.edu
+      mklame\100atxeu.com
+      nbpwie102\100sneakemail.com
+      nwoods\100mail.com
+      nkriege\100hotmail.com
+      pfc.pille\100gmx.net
+      rcolli2\100tampabay.rr.com
+      solo\100gatelys.com
+      starr\100starrnhorne.com
+      zachary\100panandscan.com
+    )
 
-  ADDRESSES_WHOSE_CONTRIBUTOR_PREFER_TO_REMAIN_UNRESOLVED = %W(
-    lagroue\100free.fr
-  )
+    ADDRESSES_WHOSE_CONTRIBUTORS_PREFER_TO_REMAIN_UNRESOLVED = %W(
+      lagroue\100free.fr
+    )
+  end
+  
+  module GithubUsernames
+    # I sent an internal message to these people asking for confirmation or full names.
+    WAITING_FOR = %w(
+      amishyn
+      Andreas
+      blackanger
+      dacat
+      dan
+      David
+      ian
+      jamesh
+      jerome
+      jonathan
+      Kane
+      Kent
+      krishna
+      mat
+      Paul
+      redinger
+    )
+    
+    COMMITS_ARE_NOT_THEIRS = [
+      'Caleb', # but wrote to Caleb Tennis, waiting for
+      'adam'
+    ]
+  end
 
   # Returns a set with all (canonical) contributor names known by the application.
   def self.all_names
@@ -314,6 +343,7 @@ module NamesManager
   map 'Ian White',                  "ian.w.white\100gmail.com"
   map 'Isaac Feliu',                'isaacfeliu'
   map 'Isaac Reuben',               "isaac\100reuben.com"
+  map 'Iván Vega',                  'ivanvr'
   map 'Jack Christensen',           "jackc\100hylesanderson.com"
   map 'Jack Danger Canty',          'danger', 'Danger'
   map 'Jacob Atzen',                'jacobat'
@@ -482,6 +512,7 @@ module NamesManager
   map 'Mark Somerville',            'Spakman'
   map 'Mark Van Holstyn',           'lotswholetime'
   map 'Markus Roberts',             "MarkusQ\100reality.com"
+  map 'Marten Veldthuis',           'Marten'
   map 'Martin Emde',                "zraii\100comcast.net", "martin.emde\100gmail.com"
   map 'Masashi Shimbo',             "shimbo\100is.naist.jp"
   map 'Marshall Roch',              'mroch'
@@ -527,6 +558,7 @@ module NamesManager
   map 'Mikel Lindsaar',             'mikel', 'raasdnil'
   map 'Miklós Fazekas',             'mfazekas'
   map 'Mislav Marohnić',            'mislav', 'mislaw', "mislav\100nippur.irb.hr"
+  map 'Moses Hohman',               'moses'
   map 'Murray Steele',              'h-lame'
   map 'Nathan Weizenbaum',          'Nex3'
   map 'Nathaniel Talbott',          'ntalbott'
