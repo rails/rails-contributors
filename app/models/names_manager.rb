d88afa398bcabcf00997907b04589a6c8c66a26d
Xavier Noria <fxn@hashref.com>
2009-11-29 15:22:57 +0100

    maps email address of Nathaniel Talbott

--- app/models/names_manager.rb
+++ app/models/names_manager.rb
@@ -505,6 +505,7 @@ module NamesManager
   map 'Mislav Marohnić',            'mislav', 'mislaw', "mislav\100nippur.irb.hr"
   map 'Murray Steele',              'h-lame'
   map 'Nathan Weizenbaum',          'Nex3'
+  map 'Nathaniel Talbott',          'ntalbott'
   map 'Neil Wilson',                'NeilW'
   map 'Nicholas Lee',               "emptysands\100gmail.com"
   map 'Nicholas Seckar',            'Ulysses'
