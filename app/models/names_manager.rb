c6779b42f93d0114b9753bb75d59e705e6615f01
Xavier Noria <fxn@hashref.com>
2009-11-29 01:06:13 +0100

    maps handler of Jason Garber

--- app/models/names_manager.rb
+++ app/models/names_manager.rb
@@ -315,6 +315,7 @@ module NamesManager
   map 'Jan Krutisch',               'halfbyte'
   map 'Jan Prill',                  "JanPrill\100blauton.de"
   map 'Jan Schwenzien',             'jeanmartin'
+  map 'Jason Garber',               'jgarber'
   map 'Jason Frey',                 'Jason Frey (Fryguy)'
   map 'Jason Ketterman',            'anshkakashi'
   map 'Jason L Perry',              'ambethia'
