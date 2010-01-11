c1cde9955b15767d24f97fa71e8b6ffe78479ae2
Xavier Noria <fxn@hashref.com>
2010-01-11 18:15:40 +0100

    requires the newrelic_rpm gem

--- config/environment.rb
+++ config/environment.rb
@@ -20,6 +20,7 @@ Rails::Initializer.run do |config|
   # config.gem "sqlite3-ruby", :lib => "sqlite3"
   # config.gem "aws-s3", :lib => "aws/s3"
   config.gem 'grit', :source => 'http://gems.github.com'
+  config.gem 'newrelic_rpm'
 
   # Only load the plugins named here, in the order given (default is alphabetical).
   # :all can be used as a placeholder for all plugins not explicitly named
