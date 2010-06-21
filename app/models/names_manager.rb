00b43ea2338ce1d2f6086d0912ad168a3b0c9a80
Xavier Noria <fxn@hashref.com>
2010-06-21 16:31:13 +0200

    maps handlers of Keith Gautreaux, now for real

--- app/models/names_manager.rb
+++ app/models/names_manager.rb
@@ -497,6 +497,7 @@ module NamesManager
   map 'Kaspar Schiess',             "eule\100space.ch"
   map 'Kazuhiro Yoshdia',           "moriq\100moriq.com", 'moriq', "kazuhiko\100fdiary.net"
   map 'Keegan Quinn',               "keegan\100thebasement.org"
+  map 'Keith Gautreaux',            'kaygee', 'kgautreaux'
   map 'Keith Morrison',             "keithm\100infused.org"
   map 'Ken Barker',                 "ken.barker\100gmail.com"
   map 'Ken Kunz',                   "kennethkunz\100gmail.com"
