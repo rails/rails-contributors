ec291dfa361af0f46a5ab776efaf64c9e0f54050
Xavier Noria <fxn@hashref.com>
2010-07-09 16:33:47 +0200

    adds a handler of Sudara Williams

--- app/models/names_manager.rb
+++ app/models/names_manager.rb
@@ -755,6 +755,7 @@ module NamesManager
   map 'Steven Bristol',             'stevenbristol'
   map 'Steven Soroka',              "ssoroka78\100gmail.com", 'ssoroka'
   map 'Stian Grytøyr',              "stian\100grytoyr.net"
+  map 'Sudara Williams',            'Sudara'
   map 'Sur Max',                    'sur'
   map 'Surendra Singhi',            "ssinghi\100kreeti.com"
   map 'Sven Klemm',                 "sven\100c3d2.de"
