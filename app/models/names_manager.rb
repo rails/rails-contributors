1dee42d2a160d5d5df23da36cfcaa4fbe77523d8
Rémy Coutable <rymai@rymai.com>
2010-08-11 00:05:47 +0800

    Add mappings for rymai

--- app/models/names_manager.rb
+++ app/models/names_manager.rb
@@ -677,7 +677,7 @@ module NamesManager
   map 'Raphael Bauduin',            "raphinou\100yahoo.com"
   map 'Rein Henrichs',              "ReinH"
   map "Remco van 't Veer",          'remvee'
-  map 'Rémy Coutable',              'remy'
+  map 'Rémy Coutable',              'rymai', 'remy', "rymai\100rymai.com", "remy.coutable\100gmail.com"
   map 'Rick Bradley',               "rick\100rickbradley.com"
   map 'Rich Collins',               'richcollins', "richcollins\100gmail.com"
   map 'Rick DeNatale',              'RubyRedRick'
