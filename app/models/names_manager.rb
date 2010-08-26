83be07f7cefabd9015f0fb7bbfae09fbc0214caf
Xavier Noria <fxn@hashref.com>
2010-08-26 20:47:04 +0200

    maps bensie as handler of James Miller

--- app/models/names_manager.rb
+++ app/models/names_manager.rb
@@ -402,6 +402,7 @@ module NamesManager
   map 'James Lindley',              'jlindley'
   map 'James Mead',                 'floehopper'
   map 'James Megquier',             "james\100slashetc.com"
+  map 'James Miller',               'bensie'
   map 'James Strachan',             "jstrachan"
   map 'Jamie Hill',                 'jamie'
   map 'Jamie Macey',                "jamie\100bravenet.com", "maceywj\100telus.net"
