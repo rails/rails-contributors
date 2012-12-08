0bc92508f82d531a5393dc3910527b14608ff6b6
Xavier Noria <fxn@hashref.com>
2012-12-08 19:07:49 +0100

    adds some magic encoding comments

--- test/unit/names_manager_test.rb
+++ test/unit/names_manager_test.rb
@@ -1,3 +1,5 @@
+# encoding: utf-8
+
 require 'test_helper'
 
 class NamesManagerTest < ActiveSupport::TestCase
--- test/unit/parameterize_test.rb
+++ test/unit/parameterize_test.rb
@@ -1,3 +1,5 @@
+# encoding: utf-8
+
 require 'test_helper'
 
 class ParameterizeTest < ActiveSupport::TestCase
