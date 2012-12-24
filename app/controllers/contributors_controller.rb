4b08f46619da418dc8c6fce13a3f654623329581
Xavier Noria <fxn@hashref.com>
2012-12-24 23:06:00 +0100

    removes spurious action

--- app/controllers/contributors_controller.rb
+++ app/controllers/contributors_controller.rb
@@ -1,7 +1,4 @@
 class ContributorsController < ApplicationController
-  before_filter :set_release, only: 'in_release'
-  before_filter :set_time_constraints, only: 'in_time_window'
-
   def index
     @contributors = if params[:release_id].present?
       set_release
@@ -12,12 +9,8 @@ class ContributorsController < ApplicationController
   end
 
   def in_time_window
+    set_time_constraints
     @contributors = Contributor.all_with_ncontributions_by_date(@since)
-    render 'index'
-  end
-
-  def in_release
-    @contributors =
     render 'index'
   end
 end
