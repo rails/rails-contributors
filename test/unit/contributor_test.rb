40b842c60559aade93ded2f64ec079499936daee
Xavier Noria <fxn@hashref.com>
2012-12-26 13:15:52 +0100

    adds test coverage for Contributor

--- test/unit/contributor_test.rb
+++ test/unit/contributor_test.rb
@@ -1,8 +1,18 @@
 require 'test_helper'
 
 class ContributorTest < ActiveSupport::TestCase
-  # Replace this with your real tests.
-  test "the truth" do
-    assert true
+  def test_the_name_writer_sets_url_id
+    c = Contributor.new(name: 'Jeremy Kemper')
+    assert_equal 'jeremy-kemper', c.url_id
+  end
+
+  def test_to_param_returns_the_url_id
+    c = contributors(:jeremy)
+    assert_equal c.url_id, c.to_param
+  end
+
+  def test_find_by_param
+    c = contributors(:jeremy)
+    assert_equal c, Contributor.find_by_param(c.to_param)
   end
 end
