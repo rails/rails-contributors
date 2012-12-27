06a41d2f5bb488361f8b1e08cf54844fcdbb997c
Xavier Noria <fxn@hashref.com>
2012-12-27 23:40:39 +0100

    removes obsolete route

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,6 @@ RailsContributors::Application.routes.draw do
 
   resources :contributors, only: 'index' do
     with_options(via: 'get') do |_|
-      _.match 'in-time-window/:time_window'         => 'contributors#in_time_window', as: 'in_time_window'
       _.match 'commits/in-time-window/:time_window' => 'commits#in_time_window',      as: 'commits_in_time_window'
       _.match 'commits/in-release/:release_id'      => 'commits#in_release',          as: 'commits_in_release'
     end
