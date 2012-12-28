2b7b4d9f4f2c7c5cf956fcca63f17f7c947cc29b
Xavier Noria <fxn@hashref.com>
2012-12-28 14:20:50 +0100

    revises ancient deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,8 +1,10 @@
+# This is raw SSH via Capistrano. TODO: Modernize this shit.
+
 set :user, 'rails'
-role :production, "contributors.rubyonrails.org"
+role :production, 'contributors.rubyonrails.org'
 
 def execute_in_rc(method, command)
-  send(method, "export PATH=/home/rails/bin:/usr/local/bin:/usr/bin:/bin; export RAILS_ENV=production; cd /home/rails/rails-contributors; #{command}")
+  send(method, "export RAILS_ENV=production; cd ~/rails-contributors; #{command}")
 end
 
 def run_in_rc(command)
@@ -15,55 +17,46 @@ end
 
 namespace :rc do
   task :tail, :roles => :production do
-    stream_in_rc "tail -f log/production.log"
+    stream_in_rc 'tail -f log/production.log'
   end
 
   task :pull, :roles => :production do
-    run_in_rc "git pull"
+    run_in_rc 'git pull'
   end
 
   task :bundle, :roles => :production do
-    run_in_rc 'bundle install --path ~/.bundle'
+    run_in_rc 'bundle install'
   end
 
-  task :update_repo, :roles => :production do
-    run_in_rc %{bundle exec rails runner 'Repo.update("/home/rails/master", "origin/master", "origin/2-3-stable", "origin/3-0-stable", "origin/3-1-stable", "origin/3-2-stable")'}
+  task :repo_sync, :roles => :production do
+    run_in_rc 'bundle exec rails runner Repo.sync'
   end
 
   task :precompile_assets, :roles => :production do
-    run_in_rc %{bundle exec rake assets:precompile}
+    run_in_rc 'bundle exec rake assets:precompile'
   end
 
-  task :delete_all_contributions, :roles => :production do
-    run_in_rc %{bundle exec rails runner 'Contribution.delete_all'}
-  end
-
-  task :restart, :roles => :production do
-    run_in_rc "touch tmp/restart.txt"
+  task :restart_unicorn, :roles => :production do
+    run_in_rc 'config/unicorn.sh restart'
   end
 
   task :expire_caches, :roles => :production do
-    # Inspired by John Leach's
-    # http://blog.brightbox.co.uk/posts/expiring-an-entire-page-cache-tree-atomically
-    expired_cache = "tmp/expired_cache.#{Time.now.to_f}"
-    run_in_rc "mv tmp/cache #{expired_cache}"
-    run_in_rc "rm -rf #{expired_cache}"
+    run_in_rc 'ApplicationUtils.expire_cached_pages'
   end
 
-  task :deploy do
+  task :deploy, :roles => :production do
     pull
     bundle
+    repo_sync
     precompile_assets
-    update_repo
-    restart
-    expire_caches
+    restart_unicorn
   end
 
   task :maintenance_on, :roles => :production do
-    run_in_rc "cp public/system/maintenance.html.deleteme public/system/maintenance.html"
+    run_in_rc 'cp public/system/maintenance.html.deleteme public/system/maintenance.html'
   end
 
   task :maintenance_off, :roles => :production do
-    run_in_rc "rm public/system/maintenance.html"
+    run_in_rc 'rm public/system/maintenance.html'
   end
 end
