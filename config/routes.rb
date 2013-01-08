7c0db9cc137af3b92a2e11ee712cd7e4ccf9a9fb
Xavier Noria <fxn@hashref.com>
2013-01-08 22:13:29 +0100

    simplifies routes with get

--- config/routes.rb
+++ config/routes.rb
@@ -1,21 +1,20 @@
 RailsContributors::Application.routes.draw do
-  match 'contributors/in-time-window/:time_window' => 'contributors#in_time_window', as: 'contributors_in_time_window', via: 'get'
+  get 'contributors/in-time-window/:time_window' => 'contributors#in_time_window', as: 'contributors_in_time_window'
 
   resources :contributors, only: 'index' do
-    with_options(via: 'get') do |_|
-      _.match 'commits/in-time-window/:time_window' => 'commits#in_time_window', as: 'commits_in_time_window'
-      _.match 'commits/in-release/:release_id'      => 'commits#in_release',     as: 'commits_in_release'
-      _.match 'commits/in-edge'                     => 'commits#in_edge',        as: 'commits_in_edge'
-    end
+    get 'commits/in-time-window/:time_window' => 'commits#in_time_window', as: 'commits_in_time_window'
+    get 'commits/in-release/:release_id'      => 'commits#in_release',     as: 'commits_in_release'
+    get 'commits/in-edge'                     => 'commits#in_edge',        as: 'commits_in_edge'
+
     resources :commits, only: 'index'
   end
 
   resources :releases, only: 'index' do
-    resources :commits, only: 'index'
+    resources :commits,      only: 'index'
     resources :contributors, only: 'index'
   end
 
-  match 'edge/contributors' => 'contributors#in_edge', as: 'contributors_in_edge', via: 'get'
+  get 'edge/contributors' => 'contributors#in_edge', as: 'contributors_in_edge'
 
   root to: 'contributors#index'
 end
