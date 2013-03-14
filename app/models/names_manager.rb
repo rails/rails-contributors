aea3d902e1b6f1dd4e231211ce07c2a03c66fcdd
Vijay Dev <vijaydev.cse@gmail.com>
2013-03-15 01:02:20 +0530

    libin is Lebin Sebastian F

--- app/models/names_manager.rb
+++ app/models/names_manager.rb
@@ -643,6 +643,7 @@ module NamesManager
   map 'Lars Pind',                  "lars\100pinds.com", "lars\100pind.com", 'Lars pind', 'lars pind'
   map 'Laurel Fan',                 "laurel\100gorgorg.org", 'laurelfan'
   map 'Lawrence Pit',               'lawrence'
+  map 'Lebin Sebastian F',          'libin'
   map 'Lee Jensen',                 'outerim'
   map 'Lee Marlow',                 'lmarlow', "lmarlow\100yahoo.com"
   map "Lee O'Mara",                 "lee\100omara.ca"
