c42048c9634edea85d9f5657ba85052115ef4778
Arthur Neves <arthurnn@gmail.com>
2014-02-20 20:14:19 -0500

    Revert "Fix test git path"
    This reverts commit 0dabf67379b316d3a7267be22eca47f29890d287.

--- test/models/repo_test.rb
+++ test/models/repo_test.rb
@@ -2,7 +2,7 @@ require 'test_helper'
 
 class RepoTest < ActiveSupport::TestCase
   def test_sync_ranks
-    Repo.new("#{Rails.root}/.git").send(:sync_ranks)
+    Repo.new.send(:sync_ranks)
 
     {jeremy: 1, david: 2, jose: 3, xavier: 3, vijay: 3}.each do |c, r|
       assert_equal r, contributors(c).rank
