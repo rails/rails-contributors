6f28d6ca48f68e211b1a3d1bbc3fab63932a8738
Xavier Noria <fxn@hashref.com>
2014-11-01 05:06:50 -0700

    adds test coverage for scrubbing in nfc

--- test/models/nfc_attribute_normalizer_test.rb
+++ test/models/nfc_attribute_normalizer_test.rb
@@ -3,6 +3,7 @@ require 'test_helper'
 class NFCAttributeNormalizerTest < ActiveSupport::TestCase
   A_GRAVE_NFC     = "\u00c0"
   A_GRAVE_NON_NFC = "\u0041\u0300"
+  NON_VALID       = "\xa9"
 
   NORMALIZER = Class.new do
     extend NFCAttributeNormalizer
@@ -48,5 +49,14 @@ class NFCAttributeNormalizerTest < ActiveSupport::TestCase
 
     @model.body = nil
     assert_nil @model.read_attribute(:body)
+  end
+
+  def test_scrubs
+    assert !NON_VALID.valid_encoding?
+
+    @model.title = NON_VALID
+
+    assert_equal '�', @model.read_attribute(:title)
+    assert @model.read_attribute(:title).valid_encoding?
   end
 end
