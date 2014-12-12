f6d6a517d64ac54345c3f7ae712b76cdf917ffdf
Xavier Noria <fxn@hashref.com>
2014-12-11 19:50:58 -0800

    triggers webhook after deploy

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -4,3 +4,9 @@ server 'contributors.rubyonrails.org', user: 'rails', roles: %w(web app db)
 set :puma_bind, 'unix:/tmp/rails-contributors.sock'
 set :puma_preload_app, false
 set :puma_workers, 2
+
+after 'deploy:finished', :trigger_webhook do
+  run_locally do
+    execute 'curl -X POST http://contributors.rubyonrails.org:8080/rails-master-hook'
+  end
+end
