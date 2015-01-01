502c4749a10eec310a6ac41ca683f33906aa2a10
Xavier Noria <fxn@hashref.com>
2015-01-01 02:50:29 -0800

    rewrites unit tests for canonical names as integration tests
    I do not care which method is able to resolve these aliases,
    all I care is that commits with these aliases get credited
    to the expected contributor name.

--- test/support/assert_contributor_names.rb
+++ test/support/assert_contributor_names.rb
@@ -1,16 +1,25 @@
 require 'set'
 
 module AssertContributorNames
-  def assert_contributor_names(sha1, *contributor_names)
+  REPO = Repo.new
+
+  def assert_contributor_names(sha1, *contributor_names, **options)
     begin
-      commit = Commit.new_from_rugged_commit(Repo.new.repo.lookup(sha1))
+      commit = Commit.new_from_rugged_commit(REPO.repo.lookup(sha1))
     rescue Rugged::OdbError
       raise "#{sha1} was not found, please make sure the local Rails checkout is up to date"
     end
 
     expected = contributor_names.to_set
-    actual   = commit.extract_contributor_names(Repo.new).to_set
+    actual   = commit.extract_contributor_names(REPO).to_set
 
-    assert_equal expected, actual
+    diff = expected - actual
+    error_message = "expected #{diff.to_a.to_sentence} to be credited for #{sha1}"
+
+    if options[:equal]
+      assert_equal expected, actual, error_message
+    else
+      assert expected.subset?(actual), error_message
+    end
   end
 end
