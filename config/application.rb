baf3ee32cf06ff57bca6d45bcc55f62ed5f6d298
Xavier Noria <fxn@hashref.com>
2016-12-07 09:01:58 -0800

    mimick Rails 5 config files
    This is a pass side-by-side to have the look as if they were generated.

--- config/application.rb
+++ config/application.rb
@@ -1,4 +1,4 @@
-require File.expand_path('../boot', __FILE__)
+require_relative 'boot'
 
 require 'active_record/railtie'
 require 'action_controller/railtie'
@@ -9,5 +9,8 @@ Bundler.require(*Rails.groups)
 
 module RailsContributors
   class Application < Rails::Application
+    # Settings in config/environments/* take precedence over those specified here.
+    # Application configuration should go into files in config/initializers
+    # -- all .rb files in that directory are automatically loaded.
   end
 end
--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,5 @@
-# Load the Rails application
-require File.expand_path('../application', __FILE__)
+# Load the Rails application.
+require_relative 'application'
 
-# Initialize the Rails application
+# Initialize the Rails application.
 Rails.application.initialize!
