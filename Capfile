65a228f6ad4283bf38dbb7b827bc5a831571b6e3
Xavier Noria <fxn@hashref.com>
2017-07-28 01:43:48 -0700

    update Capistrano config as per instructions

--- Capfile
+++ Capfile
@@ -1,6 +1,9 @@
 require 'capistrano/setup'
 require 'capistrano/deploy'
 
+require "capistrano/scm/git"
+install_plugin Capistrano::SCM::Git
+
 require 'capistrano/rvm'
 require 'capistrano/rails'
 require 'capistrano/puma'
--- config/deploy.rb
+++ config/deploy.rb
@@ -5,9 +5,6 @@ set :repo_url, 'git://github.com/fxn/rails-contributors.git'
 
 set :deploy_to, '/home/rails/rails-contributors'
 
-set :scm, :git
-set :branch, :master
-
 set :log_level, :info
 
 set :linked_files, %w{config/database.yml}
