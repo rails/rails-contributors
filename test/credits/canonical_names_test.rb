5a7680342589b335e77aeefa85b1d83f9ce63bf4
zzak <zzakscott@gmail.com>
2022-12-31 10:44:03 +0900

    use zzak canonically

--- test/credits/canonical_names_test.rb
+++ test/credits/canonical_names_test.rb
@@ -5051,6 +5051,14 @@ module Credits
       assert_contributor_names '9d8e348', 'Martin Emde'
     end
 
+    test "zzak" do
+      assert_contributor_names '9de0d59', 'zzak'
+    end
+
+    test "Zachary Scott" do
+      assert_contributor_names '3cfbee6', 'zzak'
+    end
+
     test 'zsombor' do
       assert_contributor_names '2227a17', 'Dee Zsombor'
     end
