cfc0b390bacdff68ce0ac71bfac5f9385f352708
Rafael Mendonça França <rafael@rubyonrails.org>
2025-09-09 23:35:29 +0000

    Fix nginx config

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -3,11 +3,6 @@ DOCS_SERVER_IP = '138.197.6.175'
 set :ssh_options, port: 987
 server DOCS_SERVER_IP, user: 'rails', roles: %w(web app db)
 
-set :puma_bind, 'unix:/tmp/rails-contributors.sock'
-set :puma_preload_app, false
-set :puma_workers, 1
-set :puma_phased_restart, true
-
 set :rvm_ruby_version, '3.4.5'
 set :rvm_custom_path, '/home/rails/.rvm'
 
