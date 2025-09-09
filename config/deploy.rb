df293a683ed2c4ab908542543c30655622d1bea1
Rafael Mendonça França <rafael@rubyonrails.org>
2025-09-09 13:39:06 +0000

    No need to link the puma config
    It is now in the repository.

--- config/deploy.rb
+++ config/deploy.rb
@@ -8,7 +8,7 @@ set :deploy_to, '/home/rails/rails-contributors'
 
 set :log_level, :info
 
-set :linked_files, %w{config/database.yml config/master.key config/puma/production.rb}
+set :linked_files, %w{config/database.yml config/master.key}
 set :linked_dirs, %w{log tmp/pids tmp/cache tmp/sockets public/system public/assets rails.git}
 
 set :keep_releases, 5
