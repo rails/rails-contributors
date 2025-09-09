c3295b0ff6df59000987c2135f4c27ac3ddb97d9
Rafael Mendonça França <rafael@rubyonrails.org>
2025-09-09 14:42:44 +0000

    Bind to the shared path

--- config/puma/production.rb
+++ config/puma/production.rb
@@ -1,15 +1,19 @@
 #!/usr/bin/env puma
 
+shared_path = "/home/rails/rails-contributors/shared"
+
 directory '/home/rails/rails-contributors/current'
 rackup "/home/rails/rails-contributors/current/config.ru"
 environment 'production'
 
-pidfile "/home/rails/rails-contributors/shared/tmp/pids/puma.pid"
-state_path "/home/rails/rails-contributors/shared/tmp/pids/puma.state"
-stdout_redirect '/home/rails/rails-contributors/shared/log/puma_access.log', '/home/rails/rails-contributors/shared/log/puma_error.log', true
+pidfile "#{shared_path}/tmp/pids/puma.pid"
+state_path "#{shared_path}/tmp/pids/puma.state"
+stdout_redirect "#{shared_path}/log/puma_access.log", "#{shared_path}/log/puma_error.log", true
 
 workers 2
 threads 3,3
+
+bind "unix://#{shared_path}/tmp/sockets/puma.sock"
 
 restart_command 'bundle exec puma'
 
